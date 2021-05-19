27713ae8137ccba3fbe28c18965e8aec4c664017
Gurmessa Lemma <gurmessalemma@gmail.com>
2021-05-19 13:36:53 +0300

    add test case for Blog model

--- blogs/tests.py
+++ blogs/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Blog 
 
-# Create your tests here.
+
+class BlogTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        testuser1 = User.objects.create_user(
+            username='testuser1', 
+            password='abc123'
+        )
+        testuser1.save()
+        test_blog = Blog.objects.create(
+            author=testuser1, 
+            title='Blog title', 
+            body='Body content...'
+        )
+
+    def test_blog_content(self):
+        post = Blog.objects.get(id=1)
+        author = f'{post.author}'
+        title = f'{post.title}'
+        body = f'{post.body}'
+        self.assertEqual(author, 'testuser1')
+        self.assertEqual(title, 'Blog title')
+        self.assertEqual(body, 'Body content...')

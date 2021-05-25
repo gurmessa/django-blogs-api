ed365880fbfc3f439b4f43f813add7b6183b3e48
Gurmessa Lemma <gurmessalemma@gmail.com>
2021-05-25 22:45:59 +0300

    add documentation

--- blogapi/urls.py
+++ blogapi/urls.py
@@ -16,12 +16,29 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from rest_framework import permissions # new
+from drf_yasg.views import get_schema_view # new
+from drf_yasg import openapi # new
+
+schema_view = get_schema_view( # new
+    openapi.Info(
+        title="Blog API",
+        default_version="v1",
+        description="A Blog API ",
+        terms_of_service="https://www.google.com",
+        contact=openapi.Contact(email="hi@google.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('blogs.urls')),
-    path('api-auth/', include('rest_framework.urls')),
+    path('accounts/', include('rest_framework.urls')),
     path('api/v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('', include('django.contrib.auth.urls')),
-
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
